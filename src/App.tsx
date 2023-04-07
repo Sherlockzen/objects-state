742eccfbd45160837ff6817f4b886d83470385ba
Hewerton <hewertonlourenco@gmail.com>
2023-04-07 00:12:14 -0300

    Feat: setting the basic design

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,43 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className=' flex flex-col items-center'>
+        <h1 className=' text-5xl p-24'>Formulário de Cadastro</h1>
+        <form className=' 
+        border-2 
+        border-orange-600 
+        w-2/5 
+        p-12 
+        flex 
+        flex-col 
+        gap-6'>
+          <label className=' flex flex-col'>
+            Digite seu nome
+            <input
+            className=' w-48 border border-black' 
+            name='name' />
+          </label>
+          <label className=' flex flex-col'>
+            Digite seu estado
+            <input
+            className=' w-48 border border-black' 
+            name='name' />
+          </label>
+          <label className=' flex flex-col'>
+            Digite sua cidade
+            <input
+            className=' w-48 border border-black' 
+            name='name' />
+          </label>
+
+          <button type='submit'
+          className='border border-zinc-600'
+          >
+            Salvar
+            </button>
+          
+        </form>
+      </div>  
     </div>
   )
 }
